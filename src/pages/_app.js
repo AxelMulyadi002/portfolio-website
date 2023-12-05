d4c72e727917ffff49f013012428c04904e827c6
Axel Mulyadi <azelmulyadi02@gmail.com>
2023-12-05 22:31:55 +0700

    google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,30 @@
 import '@/styles/globals.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
+import Script from 'next/script';
+
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Script
+				strategy="lazyOnload"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+
+			<Script strategy="lazyOnload">
+				{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+			</Script>
+
+			<Component {...pageProps} />
+		</>
+	);
 }
